73335e9ea4fad9326a3eb5b5978226fd2e232682
saurabh-tailor <tailorsaurabh12@gmail.com>
2024-11-14 18:24:36 +0530

    added gtag id

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 
 import "./globals.css";
 import { ThemeProvider } from "./provider";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,7 +22,22 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         <link rel="icon" href="/favicon.png" sizes="any" />
-      </head>
+      </head><Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-G6Q3ETVY22"
+        strategy="afterInteractive"
+      />
+
+      {/* Initialize Google Analytics */}
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag() {
+            dataLayer.push(arguments);
+          }
+          gtag('js', new Date());
+          gtag('config', 'G-G6Q3ETVY22');
+        `}
+      </Script>
       <body className={`${inter.className} bg-black-100`}>
         <ThemeProvider
           attribute="class"
