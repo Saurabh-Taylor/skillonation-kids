2f3f4bbca3f80dc51cb6cf20575c4168a38d436c
saurabh-tailor <tailorsaurabh12@gmail.com>
2024-11-14 16:23:58 +0530

    price and date change

--- components/Details.tsx
+++ components/Details.tsx
@@ -7,11 +7,19 @@ const Details = () => {
         <h1 className="text-purple text-xl font-semibold">
           Learning Investment
         </h1>
-        <p>&#8377; 3900/-</p>
+        <div className="flex gap-2 items-center">
+          <span className=" text-gray-500  font-medium relative">
+            <span className="before:content-[''] before:absolute before:left-0 before:right-0 before:top-1/2 before:border-t-2 before:border-[#cbacf9] before:-rotate-12">
+              &#8377; 5000/-
+            </span>
+          </span>
+          <span className=" font-semibold">&#8377; 3900/-</span>
+        </div>
+
       </div>
       <div className="flex flex-col justify-center items-center gap-2 sm:flex-1 sm:px-4 border-b-2 sm:border-b-0 sm:border-r-2 sm:pb-0 pb-4 sm:pt-0 pt-4">
         <h1 className="text-purple text-xl font-semibold">Start Date</h1>
-        <p>17 Nov. 2024</p>
+        <p>20 Nov. 2024</p>
       </div>
       <div className="flex flex-col justify-center items-center gap-2 sm:flex-1 sm:pl-4 border-b-2 sm:border-b-0 sm:pb-0 pb-4 sm:pt-0 pt-4">
         <h1 className="text-purple text-xl font-semibold">Who can Join?</h1>
